7dd6391f5fa772c3043d2c308ee78a79c5f302db
Cheri <mightypillow@tutanota.com>
2021-01-15 02:43:47 -0500

    Clean up files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## The Odin Project: Etch-A-Sketch
+
+Every hover adds 10% of black to the box
--- etch.js
+++ etch.js
@@ -3,7 +3,6 @@ const clearBtn = document.querySelector("#clear");
 
 function clearPad() {
   let gridSize = prompt("Enter number of squares per side (Min: 2, Max: 100, Default: 16");
-  console.log(`gridSize 1: ${gridSize}`);
   if(!Number.isInteger(Number(gridSize)) || gridSize === null) {
     gridSize = 16;
   }
@@ -13,8 +12,6 @@ function clearPad() {
   else if(gridSize > 100) {
     gridSize = 100;
   }
-
-  console.log(`gridSize 2: ${gridSize}`);
 
   while(sketchPad.firstChild) {
     sketchPad.removeChild(sketchPad.firstChild);
