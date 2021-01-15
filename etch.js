704a2a20585808ad4a47e717bbce24314f9b81cc
Cheri <mightypillow@tutanota.com>
2021-01-15 01:59:49 -0500

    Add button that clears and makes a new grid

--- etch.js
+++ etch.js
@@ -1,8 +1,39 @@
 const sketchPad = document.querySelector("#sketchpad");
+const clearBtn = document.querySelector("#clear");
 
-for(let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.addEventListener("mouseover", (e) => e.target.classList.add("trail"));
-  sketchPad.appendChild(square);
+function clearPad() {
+  let gridSize = prompt("Enter number of squares per side (Min: 2, Max: 100, Default: 16");
+  console.log(`gridSize 1: ${gridSize}`);
+  if(!Number.isInteger(Number(gridSize)) || gridSize === null) {
+    gridSize = 16;
+  }
+  else if(gridSize < 2) {
+    gridSize = 2;
+  }
+  else if(gridSize > 100) {
+    gridSize = 100;
+  }
+
+  console.log(`gridSize 2: ${gridSize}`);
+
+  while(sketchPad.firstChild) {
+    sketchPad.removeChild(sketchPad.firstChild);
+  }
+
+  buildPad(gridSize);
 }
+
+function buildPad(n) {
+  sketchPad.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+  sketchPad.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+  for(let i = 0; i < n * n; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.addEventListener("mouseover", (e) => e.target.classList.add("trail"));
+    sketchPad.appendChild(square);
+  }
+}
+
+clearBtn.addEventListener("click", clearPad);
+
+buildPad(16);
