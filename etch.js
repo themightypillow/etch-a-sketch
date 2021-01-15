426669ee0f27c28b22312dc1f21e72f3b502255b
Cheri <mightypillow@tutanota.com>
2021-01-15 00:58:48 -0500

    Set up hover effect on grid divs

--- etch.js
+++ etch.js
@@ -3,5 +3,6 @@ const sketchPad = document.querySelector("#sketchpad");
 for(let i = 0; i < 256; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
+  square.addEventListener("mouseover", (e) => e.target.classList.add("trail"));
   sketchPad.appendChild(square);
 }
